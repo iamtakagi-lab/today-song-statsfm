6530f14e06b5c70f8226178afe756cdf8800aa44
takagi <iam.takagi.dev@gmail.com>
2022-12-05 21:16:53 +0900

    feat: stats.fm (removed last.fm)

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
 process.env.TZ = "Asia/Tokyo"
 
-import { getTopTrackFromLastfm } from "./lastfm.js"
+import { getTopTrackFromStatsfm } from "./statsfm.js"
 import { postTweet } from "./twitter.js"
 import moment from "moment"
 import "moment/locale/ja.js"
 
 const handleMain = async () => {
   // 直近のトップソングを取得
-  const topTrack = await getTopTrackFromLastfm()
-  if (!topTrack) throw new Error("[last.fm] 曲データが存在しません ギャオオオオオオオオオオオオオオオオオオオオオオ")
-
-  const context = `${process.env.LASTFM_USER_ID ?? ''} さんの ${moment().subtract(1, 'days').format("YYYY/MM/DD")} の曲は「${topTrack.artist.name}」の「${topTrack.name}」でした #TodaySong ${topTrack.url}`
+  const data = await getTopTrackFromStatsfm()
+  if (!data) throw new Error("[stats.fm] 曲データが存在しません ギャオオオオオオオオオオオオオオオオオオオオオオ")
+  const { track } = data
+  const context = `${process.env.STATSFM_ID ?? ''} さんの ${moment().subtract(1, 'days').format("YYYY/MM/DD")} の曲は「${track.artists.map((artist) => artist.name).join(",")}」の「${track.name}」でした #TodaySong https://open.spotify.com/track/${track.externalIds.spotify[0]}`
   console.log(context)
   
-  postTweet(context)
+  //postTweet(context)
 }
 
 ;(async () => {
---
+++ src/statsfm.ts
@@ -0,0 +1,59 @@
+import fetch from "node-fetch";
+
+export const getTopTrackFromStatsfm = async () => {
+  const apiUrl = new URL(`/api/v1/users/${process.env.STATSFM_ID}/top/tracks`, 'https://api.stats.fm');
+  apiUrl.searchParams.set('range', 'weeks');
+
+  const result = await fetch(apiUrl.toString(), {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: process.env.STATSFM_TOKEN ?? ''
+    }
+  });
+  const data = await result.json();
+  if (!data) return undefined
+  console.log(data as ToptracksResnponse.RootObject)
+  return (data as ToptracksResnponse.RootObject).items[0]
+}
+
+declare module ToptracksResnponse {
+
+  export interface Album {
+      id: number;
+      image: string;
+      name: string;
+  }
+
+  export interface Artist {
+      id: number;
+      name: string;
+  }
+
+  export interface ExternalIds {
+      spotify: string[];
+  }
+
+  export interface Track {
+      albums: Album[];
+      artists: Artist[];
+      durationMs: number;
+      explicit: boolean;
+      externalIds: ExternalIds;
+      id: number;
+      name: string;
+      spotifyPopularity: number;
+  }
+
+  export interface Item {
+      position: number;
+      streams?: any;
+      indicator: string;
+      track: Track;
+  }
+
+  export interface RootObject {
+      items: Item[];
+  }
+
+}
